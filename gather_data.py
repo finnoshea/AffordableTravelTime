69fbdd79511a01d3920b41ff6b178b14d43fe1a7
Brendan O'Shea <nadnerbity@gmail.com>
2019-01-04 14:02:33 -0800

    Added the ability to search based on zip code and save the data to a pandas dataframe.  Also added the ability to save data by zipcode.

--- gather_data.py
+++ gather_data.py
@@ -2,6 +2,279 @@
 """Data collection methods and data storage.
 """
 
+# Because this file uses Firefox via selenium you may need to install
+# geckodriver.  If you have homebrew install 'brew install geckodriver' will
+# sort it out for you.  You can also download it from the github repo.  It
+# needs to be added to your path:
+# 'export PATH=$PATH:/path/to/geckodriver/'
+# See here for more details:
+# https://stackoverflow.com/questions/40208051/selenium-using-python-geckodriver-executable-needs-to-be-in-path
+
+
 __license__ = "GPL"
 __version__ = "0.0"
 __status__ = "Development"
+
+# Set up the imports to run the scrape.
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from BeautifulSoup import BeautifulSoup
+import shared_res
+import pandas as pd
+import numpy as np
+import os
+import time # gonna wanna pause to avoid captchas
+import datetime
+
+
+# Build the class that handles the website actions like searching for
+# zipcodes and changing pages.
+class zillow_zipcode_search:
+    def __init__(self):
+        # Load the firefox web driver.
+        self.driver         = webdriver.Firefox()
+        self.do_next_page   = 1
+        self.no_results     = 0
+
+    # Move to the next page in the search.
+    def next_page(self):
+        self.page_number    = self.page_number + 1
+        self.page_to_load   = self.first_page + str(self.page_number) + '_p/'
+
+    # Get the source for the current page to pass to BeautifulSoup
+    # Transfer the page source into BeautifulSoup.
+    def get_current_page(self):
+        # Initial page after zipcode search.
+        # self.page_to_load = "file://" + os.getcwd() + \
+        #                     "/saved_pages_for_testing/94025_SFH.html"
+        # self.page_to_load = "file://" + os.getcwd() + \
+        #                     "/saved_pages_for_testing/94089_SFH.html"
+        # No matching results
+        # self.page_to_load = "file://" + os.getcwd() + \
+        #                     "/saved_pages_for_testing/94025_NMR.html"
+        self.driver.get(self.page_to_load)
+        self.current_page = self.driver.page_source  # extract the source for
+        # BeautifulSoup
+        return (self.current_page)
+
+    # Run tests on the current page, you're looking for whether the page is a
+    #  captcha page or whether there are results on the current page and a
+    # few other things.
+    def test_current_page(self):
+        soup = BeautifulSoup(self.current_page)
+
+        # Test to see if there is a next page.  If there isn't make sure the
+        # navigator knows that.  If there is no next page then the result
+        # will be an empty list.
+        no_next_page = soup.findAll("li", {"class": "zsg-pagination-next"})
+        if not no_next_page:
+            self.do_next_page = 0
+
+        # Test to see if there are any results on this page.  If not,
+        # tell the iterate to stop trying to change pages.
+        no_results = soup.findAll("h3", {"class": "zsg-content_collapsed"})
+        try:
+            if no_results[0].text == 'No matching results...':
+                print("Probably the last page...")
+                self.no_results = 1
+        except IndexError:
+            1+1 # Don't do anything, keep going.  Better to print something?
+
+    # Dump the current master dataframe once the zipcode search is complete.
+    def dump_zipcode_dataframe(self, dataframe_in):
+        dump_location = os.getcwd() +  "/dumped_data/"
+        dump_filename = self.current_zip + ".csv"
+        dump_path = dump_location + dump_filename
+
+        # Load the old data and check the current list against the saved list
+        #  and only add new listings.  This might be slow.
+        try:
+            self.df = pd.read_csv(dump_path)
+            # When the dataframe is dumped zillow_id is converted to an
+            # int64.  This means you need to convert it to object so that the
+            #  types of the entries in the column match and ca be merged.
+            self.df['zillow_id'] = self.df['zillow_id'].astype('object')
+            dataframe_in = pd.merge(self.df, dataframe_in, how='outer',
+                                   on='zillow_id')
+            # dataframe_in = dataframe_in.merge(df, how='outer', on='zillow_id')
+        except IOError:
+            1+1 # Do nothing
+
+        dataframe_in.to_csv(dump_path)
+
+    def close_browser(self):
+        self.driver.close()
+
+    # This is for testing the inner functioning of the class.  Don't use it
+    # in production.
+    def testing_function(self):
+        print(instance_zillow_scrape.zillow_data_master.filter(regex="zillow_"))
+
+    # Define a search function that updates the page to load.
+    def search_zipcode(self, zipcode_in):
+        print('Currently scraping zip code: ' + zipcode_in)
+        self.page_number = 1
+        self.current_zip = zipcode_in
+        self.first_page = "https://www.zillow.com/homes/" + zipcode_in + "_rb/"
+        self.page_to_load = self.first_page
+
+        # Create an instance of the scraper
+        self.instance_zillow_scrape = zillow_parser()
+
+        # Zillow wont return more than 20 pages so use a for loop that
+        # automatically stops at 30.  If you make a mistake and it doesn't stop
+        #  earlier it won't get stuck in a while.
+        for i in range(30):
+            # Test the current page to catch last pages and captchas
+            self.get_current_page()
+            self.test_current_page()
+            if self.no_results == 1:
+                break
+            # Scrape the current page
+            self.instance_zillow_scrape.get_houses(self.current_page)
+            # Pause to avoid captchas
+            time.sleep(np.random.uniform(0, 10))
+            # Goto the next page
+            if self.do_next_page == 1:
+                self.next_page()
+            else:
+                break
+
+        # Dump the scraped data
+        self.dump_zipcode_dataframe(
+            self.instance_zillow_scrape.zillow_data_master)
+
+        # Close the web driver
+        # self.close_browser()
+
+# Build the class to collect the data from each entry on Zillow.  This class
+# takes in html from a web page and extracts the information on all the houses
+# displayed on it.
+class zillow_parser:
+    def __init__(self):
+        # Build an empty dataframe to be replaced later
+        self.zillow_data_master = pd.DataFrame(
+            columns=shared_res.pandas_column_names)
+
+    # This function extracts all the houses list on the current page,
+    # which is html as extracted from the selenium instance of the web
+    # browser.  It returns an iterable list of what zillow calls "cards" that
+    #  contain all the information you wish to extract like location and
+    # price.  When done, the data is appended to the master dataframe.
+    def get_houses(self, current_page):
+        soup = BeautifulSoup(current_page)
+        self.photo_cards = soup.findAll("article",
+                                   {"class": "zsg-photo-card photo-card "
+                                             "zsg-aspect-ratio type-not-favorite"})
+        # Build the pandas data frame to hold the extracted data
+        self.zillow_data = pd.DataFrame(
+            index = range(len(self.photo_cards)),
+            columns=shared_res.pandas_column_names)
+
+        self.iterate_over_house_on_page()
+
+    # Grab longitude and latitude
+    def get_location(self, card_entry):
+        # Grab longitude and latitude
+        temp = card_entry.findAll("meta", {"itemprop": "latitude"})
+        # print("Latitude : " + temp[0]['content'])
+        lat = temp[0]['content']
+        temp = card_entry.findAll("meta", {"itemprop": "longitude"})
+        # print("Longitude : " + temp[0]['content'])
+        longi = temp[0]['content']
+        return(lat, longi)
+
+    # Grab the price and the address, which are contained in the card caption
+    def get_card_caption(self, card_entry):
+        card_caption = card_entry.findAll("div", {"class"
+                                                      : "zsg-photo-card-caption"})
+        price = card_entry.findAll("span",
+                                        {"class": "zsg-photo-card-price"})
+        # If the listing doesn't have a price then "price" will be empty.
+        # Set up a try to avoid this case.
+        try:
+            price = price[0].text
+            price = price[1:].replace(',', '')  # Get rid of $ and commas
+        except IndexError:
+            price = 0
+
+        address = card_entry.findAll("span", {"class" :
+                                                  "zsg-photo-card-address"})
+        address = address[0].text
+        return(price, address)
+
+    # Get the address, this has the information nicely divided unlike the
+    # card caption
+    def get_address(self, card_entry):
+        # Grab longitude and latitude
+        temp = card_entry.findAll("span", {"itemprop": "streetAddress"})
+        addressStreet = temp[0].text
+        temp = card_entry.findAll("span", {"itemprop": "addressLocality"})
+        addressCity = temp[0].text
+        temp = card_entry.findAll("span", {"itemprop": "addressRegion"})
+        addressState = temp[0].text
+        temp = card_entry.findAll("span", {"itemprop": "postalCode"})
+        zipcode = temp[0].text
+
+        return(zipcode, addressStreet, addressCity, addressState)
+
+    # Get the home type, i.e. manufactured, apartement, whatever.
+    # All of the data you captured above is listed here, so you maybe be able
+    #  to redo this whole class with one function call.
+    def get_home_type(self, card_entry):
+        homeType = "0"
+        temp = card_entry.findAll("div", {"class": "minibubble template hide"})
+        # Convert the output to a string that can be split.
+        temp = str(temp[0]).split(',')
+        # The location of the hometype entry changes sometimes, so you have
+        # to go through the split list.  Yikes.
+        for j in temp:
+            if (j[1:9] == "homeType"):
+                homeType = j.split(':')[1]
+                homeType = homeType.strip(' ').strip('"')
+
+        return(homeType)
+
+    # This iterates over the card captions and extracts the listing data.
+    def iterate_over_house_on_page(self):
+        m = 0
+        for k in self.photo_cards:
+            # Extract the longitude and latitude
+            lat, longi = self.get_location(k)
+            price, address = self.get_card_caption(k)
+            zipcode, addressStreet, addressCity, addressState = \
+                self.get_address(k)
+            homeType = self.get_home_type(k)
+
+            # Drop the information into the dataframe.  I want to keep all
+            # the pushing of data into the dataframe in one location to make
+            # updates easier.
+            self.zillow_data['zillow_id'][m] = k.get('data-zpid')
+            self.zillow_data['zillow_status'][m] = k.get('data-pgapt')
+            self.zillow_data['zillow_latitude'][m] = lat
+            self.zillow_data['zillow_longitude'][m] = longi
+            self.zillow_data['zillow_price'][m] = price
+            self.zillow_data['zillow_addressStreet'][m] = address
+            self.zillow_data['zillow_zipcode'][m] = zipcode
+            self.zillow_data['zillow_addressStreet'][m] = addressStreet
+            self.zillow_data['zillow_addressCity'][m] = addressCity
+            self.zillow_data['zillow_addressState'][m] = addressState
+            self.zillow_data['zillow_homeType'][m] = homeType
+            self.zillow_data['date_scraped'][m] = datetime.datetime.now().isoformat()
+            # Update the iterator
+            m = m + 1
+
+        # Write the data to the master dataframe.
+        self.zillow_data_master = self.zillow_data_master.append(
+                self.zillow_data, ignore_index=True)
+
+# SBuild the instances needed to perform a zipcode search
+some_zillow_zipcode_search = zillow_zipcode_search()
+
+# Run a search for a zipcode
+# some_zillow_zipcode_search.search_zipcode('94027')
+
+for H in shared_res.napa_county_zip:
+    some_zillow_zipcode_search.search_zipcode(H)
+
+
--- shared_res.py
+++ shared_res.py
@@ -8,8 +8,403 @@ __status__ = "Development"
 
 
 # names of the columns in the pandas dataframe
-pandas_column_names = ['zillow_address','zillow_features','zillow_price',
-'google_start_address','google_start_location','google_end_address',
-'google_end_location','morning_drive_duration','morning_drive_duration_with_traffic',
-'evening_drive_duration','evening_drive_duration_with_traffic',
-'morning_transit_duration','evening_transit_duration']
+pandas_column_names = ['zillow_id',
+                       'zillow_addressStreet',
+                       'zillow_addressCity',
+                       'zillow_addressState',
+                       'zillow_zipcode',
+                       'zillow_features',
+                       'zillow_price',
+                       'zillow_longitude',
+                       'zillow_latitude',
+                       'zillow_status',
+                       'zillow_homeType',
+                       'date_scraped',
+                       'location',
+                       'google_start_address',
+                       'google_start_location',
+                       'google_end_address',
+                       'google_end_location',
+                       'morning_drive_duration',
+                       'morning_drive_duration_with_traffic',
+                       'evening_drive_duration',
+                       'evening_drive_duration_with_traffic',
+                       'morning_transit_duration',
+                       'evening_transit_duration']
+
+
+# Grabbed from http://www.city-data.com/county/Santa_Clara_County-CA.html
+santa_clara_county_zip ='\
+94022 \
+94023 \
+94024 \
+94040 \
+94041 \
+94042 \
+94043 \
+94085 \
+94086 \
+94087 \
+94089 \
+94301 \
+94304 \
+94305 \
+94306 \
+95002 \
+95008 \
+95014 \
+95020 \
+95030 \
+95031 \
+95032 \
+95033 \
+95035 \
+95037 \
+95038 \
+95046 \
+95050 \
+95051 \
+95054 \
+95056 \
+95070 \
+95108 \
+95110 \
+95111 \
+95112 \
+95113 \
+95116 \
+95117 \
+95118 \
+95119 \
+95120 \
+95121 \
+95122 \
+95123 \
+95124 \
+95125 \
+95126 \
+95127 \
+95128 \
+95129 \
+95130 \
+95131 \
+95132 \
+95133 \
+95134 \
+95135 \
+95136 \
+95138 \
+95139 \
+95140 \
+95148 \
+95150 \
+95151 \
+95158 \
+95160 \
+95164'
+santa_clara_county_zip = santa_clara_county_zip.split(' ')
+
+
+# http://www.city-data.com/county/Santa_Cruz_County-CA.html
+santa_cruz_county_zip = '\
+95001 \
+95003 \
+95005 \
+95006 \
+95010 \
+95017 \
+95018 \
+95019 \
+95060 \
+95061 \
+95062 \
+95063 \
+95064 \
+95065 \
+95066 \
+95067 \
+95073 \
+95076'
+santa_cruz_county_zip = santa_cruz_county_zip.split(' ')
+
+# http://www.city-data.com/county/San_Mateo_County-CA.html
+san_mateo_county_zip = '\
+94002 \
+94005 \
+94010 \
+94014 \
+94015 \
+94018 \
+94019 \
+94020 \
+94021 \
+94025 \
+94026 \
+94027 \
+94028 \
+94029 \
+94030 \
+94037 \
+94038 \
+94044 \
+94060 \
+94061 \
+94062 \
+94063 \
+94065 \
+94066 \
+94070 \
+94074 \
+94080 \
+94128 \
+94303 \
+94401 \
+94402 \
+94403 \
+94404'
+san_mateo_county_zip = san_mateo_county_zip.split(' ')
+
+# http://www.city-data.com/county/San_Francisco_County-CA.html
+san_francisco_county_zip ='\
+94101 \
+94102 \
+94103 \
+94104 \
+94105 \
+94107 \
+94108 \
+94109 \
+94110 \
+94111 \
+94112 \
+94114 \
+94115 \
+94116 \
+94117 \
+94118 \
+94119 \
+94121 \
+94122 \
+94123 \
+94124 \
+94127 \
+94129 \
+94131 \
+94132 \
+94133 \
+94134 \
+94137 \
+94142 \
+94188'
+san_francisco_county_zip = san_francisco_county_zip.split(' ')
+
+# http://www.city-data.com/county/Alameda_County-CA.html
+alameda_county_zip = '\
+94501 \
+94502 \
+94536 \
+94538 \
+94539 \
+94541 \
+94542 \
+94544 \
+94545 \
+94546 \
+94550 \
+94551 \
+94552 \
+94555 \
+94560 \
+94566 \
+94568 \
+94577 \
+94578 \
+94579 \
+94580 \
+94582 \
+94586 \
+94587 \
+94588 \
+94601 \
+94602 \
+94603 \
+94605 \
+94606 \
+94607 \
+94608 \
+94609 \
+94610 \
+94611 \
+94612 \
+94618 \
+94619 \
+94621 \
+94650 \
+94702 \
+94703 \
+94704 \
+94705 \
+94706 \
+94707 \
+94708 \
+94709 \
+94710'
+alameda_county_zip = alameda_county_zip.split(' ')
+
+# http://www.city-data.com/county/Contra_Costa_County-CA.html
+contra_costa_county_zip = '\
+94506 \
+94507 \
+94509 \
+94511 \
+94513 \
+94514 \
+94516 \
+94517 \
+94518 \
+94519 \
+94520 \
+94521 \
+94523 \
+94524 \
+94525 \
+94526 \
+94528 \
+94530 \
+94531 \
+94547 \
+94549 \
+94553 \
+94556 \
+94561 \
+94563 \
+94564 \
+94565 \
+94583 \
+94595 \
+94596 \
+94597 \
+94598 \
+94801 \
+94803 \
+94804 \
+94805 \
+94806'
+contra_costa_county_zip = contra_costa_county_zip.split(' ')
+
+# http://www.city-data.com/county/Marin_County-CA.html
+marin_county_zip = '\
+94901 \
+94903 \
+94904 \
+94920 \
+94924 \
+94925 \
+94930 \
+94933 \
+94937 \
+94939 \
+94941 \
+94942 \
+94945 \
+94946 \
+94947 \
+94949 \
+94950 \
+94956 \
+94957 \
+94960 \
+94965 \
+94970 \
+94973 \
+94979'
+marin_county_zip = marin_county_zip.split(' ')
+
+# http://www.city-data.com/county/Solano_County-CA.html
+solano_county_zip ='\
+94510 \
+94533 \
+94534 \
+94571 \
+94585 \
+94589 \
+94590 \
+94591 \
+95620 \
+95687 \
+95688'
+solano_county_zip = solano_county_zip.split(' ')
+
+# http://www.city-data.com/county/Sonoma_County-CA.html
+sonoma_county_zip ='\
+94927 \
+94928 \
+94931 \
+94951 \
+94952 \
+94954 \
+95401 \
+95402 \
+95403 \
+95404 \
+95405 \
+95407 \
+95409 \
+95416 \
+95425 \
+95436 \
+95439 \
+95442 \
+95446 \
+95448 \
+95452 \
+95471 \
+95472 \
+95473 \
+95476 \
+95486 \
+95492 \
+95497'
+sonoma_county_zip = sonoma_county_zip.split(' ')
+
+# http://www.city-data.com/county/Napa_County-CA.html
+napa_county_zip = '\
+94503 \
+94508 \
+94515 \
+94558 \
+94559 \
+94574 \
+94581 \
+94599'
+napa_county_zip = napa_county_zip.split(' ')
+
+# http://www.city-data.com/county/San_Joaquin_County-CA.html
+san_joaquin_county_zip ='\
+95204 \
+95205 \
+95206 \
+95207 \
+95209 \
+95210 \
+95212 \
+95215 \
+95219 \
+95220 \
+95227 \
+95236 \
+95237 \
+95240 \
+95242 \
+95258 \
+95269 \
+95290 \
+95304 \
+95320 \
+95330 \
+95336 \
+95337 \
+95366 \
+95376 \
+95377'
+san_joaquin_county_zip = san_joaquin_county_zip.split(' ')
