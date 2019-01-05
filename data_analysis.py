a1ceba3d74b1e9d07e4cb19f759af57faacd4e63
Brendan O'Shea <nadnerbity@gmail.com>
2019-01-05 13:43:43 -0800

    Small bug fixes

--- data_analysis.py
+++ data_analysis.py
@@ -5,3 +5,13 @@
 __license__ = "GPL"
 __version__ = "0.0"
 __status__ = "Development"
+
+# Principal is the initial loan amount.
+# interest is the MONTHLY interest.  i.e. is the yearly interst is 4% then
+# interest = 0.04/12
+# months is the length of the loans in months.
+def calculate_monthly_payment(principal, interest, months):
+    monthly_payment = principal * \
+                      (interest * (1 + interest)**months) / \
+                      ((1+interest)**months - 1)
+    return(monthly_payment)
